2a993b2ab4cdb3e972bb43bd9830e6e8e044dc9d
paulabannerman <the.dcartist@gmail.com>
2020-05-11 19:40:01 -0400

    Updates

---
+++ precache-manifest.4d63616ff633feea30e3b1ca75d53ea1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8d02957fd1e5d83939f6f130db245f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fad39a4942ec91cc3fe2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "27ae8c55e34fbf2924ff",
+    "url": "/static/js/2.8e64a94b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e64a94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad39a4942ec91cc3fe2",
+    "url": "/static/js/main.a028c25b.chunk.js"
+  },
+  {
+    "revision": "f99bb64b52d1a6aed259",
+    "url": "/static/js/runtime-main.86940c6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
